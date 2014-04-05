b4d4e840a7076db974fef79243c65ee0671f5d95
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-04-05 16:02:20 +0800

    Updated hardProperty procedure with multiple property hardening.

--- harden-property.js
+++ harden-property.js
@@ -1,25 +1,55 @@
 try{ var base = window; }catch( error ){ var base = exports; }
 ( function module( base ){
 	define( "hardenProperty",
+		[
+			"argumentsToArray"
+		],
 		function construct( ){
-			var hardenProperty = function hardenProperty( reference, propertyName, propertyValue ){
-				if( typeof reference != "object" 
-					&& typeof reference != "function" )
-				{
-					throw new Error( "invalid reference" );
-				}
+			var hardenProperty = function hardenProperty( ){
+				var parameters = argumentsToArray( arguments );
+				
+				var reference = parameters[ 0 ];
 
-				if( typeof propertyName != "string" ){
-					throw new Error( "invalid property name" );
-				}
+				if( typeof parameters[ 1 ] == "object" ){
+					var registryList = parameters[ 1 ];
 
-				Object.defineProperty( reference, propertyName,
+					var propertyNameList = Object.keys( registryList );
+					var propertyNameListLength = propertyNameList.length;
+					
+					if( !( propertyNameListLength > 0 ) ){
+						throw new Error( "invalid registry list" );
+					}
+
+					var propertyName;
+					var value;
+					for( var index = 0; index < propertyNameListLength; index++ ){
+						propertyName = propertyNameList[ index ];
+						value = registryList[ propertyName ];
+						hardenProperty( reference, propertyName, value );
+					}
+
+				}else if( parameters.length == 3 ){
+					var propertyName = parameters[ 1 ];
+					var value = parameters[ 2 ];
+
+					if( typeof reference != "object" 
+						&& typeof reference != "function" )
 					{
-						"enumerable": false,
-						"configurable": false,
-						"writable": false,
-						"value": value
-					} );
+						throw new Error( "invalid reference" );
+					}
+
+					if( typeof propertyName != "string" ){
+						throw new Error( "invalid property name" );
+					}
+
+					Object.defineProperty( reference, propertyName,
+						{
+							"enumerable": false,
+							"configurable": false,
+							"writable": false,
+							"value": value
+						} );
+				}
 			};
 			base.hardenProperty = hardenProperty;
 		} );
